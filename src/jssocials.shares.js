8d5b885fb74f806e8ae57db0b88b10d628951a58
Artem Tabalin <tabalinas@gmail.com>
2015-04-23 10:09:28 +0300

    Shares: Make googleplus count more robust

--- src/jssocials.shares.js
+++ src/jssocials.shares.js
@@ -30,7 +30,7 @@
                 return "http://anyorigin.com/get?url=" + window.encodeURIComponent("https://plusone.google.com/_/+1/fastbutton?url=" + this.url) + "&callback=?";
             },
             getCount: function(data) {
-                return parseFloat(data.contents.match(/\{c: ([.0-9E]+)/)[1]);
+                return parseFloat((data.contents.match(/\{c: ([.0-9E]+)/) || [])[1]);
             }
         },
 
